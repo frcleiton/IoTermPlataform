ad17ee627e78a84d7ff752094f5c5a60796688bd
frcleiton <cleitonrfereira@gmail.com>
2017-09-16 01:15:32 -0300

    Fix date time

--- ioterm_app.py
+++ ioterm_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 import time
-import datetime
+from datetime import datetime
 from email.Utils import formatdate
 from pymongo import MongoClient
 import pymongo
@@ -20,13 +20,13 @@ def read_temp_database():
 	str_time = ''
 	for ultima_leitura in leituras.find( {'topic':'MG/TI/RP01/temperature'} ).sort('t', pymongo.DESCENDING ).limit(1):
 		temperatures.append(ultima_leitura['value'])
-		str_time = ultima_leitura['t']
-		print str_time
-		print type(str_time)
+		#print ultima_leitura['t']
+		#strdatetime = datetime.now().strftime("%d-%m-%Y %H:%M")
+		strdatetime = ultima_leitura['t'].strftime("%d-%m-%Y %H:%M")
 	for ultima_leitura in leituras.find( {'topic':'MG/TI/RP01/humidity'} ).sort('t', pymongo.DESCENDING ).limit(1):
 		humidities.append(ultima_leitura['value'])
 	
-	return render_template("sensors.html",temp=temperatures,hum=humidities,str_time=str_time,equipname='RPXX',equipsite='MGXX',tmin=10,tmax=30)
+	return render_template("sensors.html",temp=temperatures,hum=humidities,str_time=strdatetime,tmin=29,tmax=30)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
---
+++ static/javascript/alerta_sensor.js
@@ -0,0 +1,6 @@
+var lmin = parseInt(document.getElementById('par_temp_min').value);
+var lmax = parseInt(document.getElementById('par_temp_max').value);
+if (True) {
+      document.getElementById("temperatura").style.color = "white";
+      document.getElementById("temperatura").style.backgroundColor = "blue";
+} 

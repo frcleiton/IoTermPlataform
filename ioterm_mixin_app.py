c70ff43d1a54d9bfc6499de551964582b2aac298
frcleiton <cleitonrferreira@gmail.com>
2017-09-30 01:33:26 -0300

    add user login

--- ioterm_mixin_app.py
+++ ioterm_mixin_app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 
 # config
 app.config.update(
-    DEBUG = True,
-    SECRET_KEY = 'i0t3rm!@#$%',
+	DEBUG = True,
+	SECRET_KEY = 'i0t3rm!@#$%',
 )
 
 # flask-login
@@ -20,22 +20,43 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 
 # silly user model
+#class User(UserMixin):
+# 	def __init__(self, id):
+#		self.id = id
+#		self.name = "user" + str(id)
+#		self.password = self.name + "_secret"
+#
+#	def __repr__(self):
+#		return "%d/%s/%s" % (self.id, self.name, self.password)
+
 class User(UserMixin):
- 	def __init__(self, id):
-		self.id = id
-		self.name = "user" + str(id)
-		self.password = self.name + "_secret"
+	"""
+	User Class for flask-Login
+	"""
+	def __init__(self, userid, password):
+		self.id = userid
+		self.password = password
 
-	def __repr__(self):
-		return "%d/%s/%s" % (self.id, self.name, self.password)
+	@staticmethod
+	def get(userid):
+		"""
+		Static method to search the database and see if userid exists.  If it
+		does exist then return a User Object.  If not then return None as
+		required by Flask-Login.
+		"""
+		#For this example the USERS database is a list consisting of
+		#(user,hased_password) of users.
+		for user in USERS:
+			if user[0] == userid:
+				return User(user[0], user[1])
+		return None
 
-# Test user
-#users = [User(id) for id in range(1, 21)]
-#users.append(User(21))
-#users = []
-#users.append(User(1))
-#print users
-
+def hash_pass(password):
+	"""
+	Return the md5 hash of the password+salt
+	"""
+	salted_password = password + app.secret_key
+	return md5.new(salted_password).hexdigest()
 
 @app.route("/")
 @login_required
@@ -60,7 +81,7 @@ def index():
 	   strdatetime.append(ultima_leitura['t'].strftime("%d-%m-%Y %H:%M"))
 	for ultima_leitura in leituras.find( {'topic':'RP02/humidity'} ).sort('t', pymongo.DESCENDING ).limit(1):
 	   humidities.append(ultima_leitura['value'])
-	   
+
 	return render_template("sensors.html",temp=temperatures,hum=humidities,str_time=strdatetime,tmin=29,tmax=30)
 
 @app.route("/hist/<sensor>")
@@ -68,7 +89,7 @@ def index():
 def historico(sensor):
 	msg = 'Sensor: ' +sensor
 
-    #Conectando ao banco MongoDB
+	#Conectando ao banco MongoDB
 	cliente = MongoClient('localhost', 27017)
 	banco = cliente.iotdata
 	leituras = banco.leituras
@@ -96,7 +117,7 @@ def historico(sensor):
 							to_date 		= to_date_str,
 							query_string	= 'select ', #This query string is used
 							#by the Plotly link
-							sensorname      = sensor.upper())
+							sensorname	  = sensor.upper())
 
 
 @app.route("/conf/<sensor>")
@@ -107,50 +128,61 @@ def configuracao(sensor):
 	paritens = ['15','30','20','50','alerta@ioterm.com.br']
 	if request.method == 'GET':
 		return render_template("settings.html",parametros=paritens,max=30,sensorname=sensor.upper())
-##    if request.method == 'POST':
-##        set_min = request.form['temp_min']
-##        curs2.execute('UPDATE parametros SET valor = %d where parametro = %s' % (int(set_min),"'LIMITE_MIN_TEMP'"))
-##        set_max = request.form['temp_max']
-##        curs2.execute('UPDATE parametros SET valor = %d where parametro = %s' % (int(set_max),"'LIMITE_MAX_TEMP'"))
-##        conn2.commit()
-##        conn2.close()
-##        return redirect('/lab_parametros')
+##	if request.method == 'POST':
+##		set_min = request.form['temp_min']
+##		curs2.execute('UPDATE parametros SET valor = %d where parametro = %s' % (int(set_min),"'LIMITE_MIN_TEMP'"))
+##		set_max = request.form['temp_max']
+##		curs2.execute('UPDATE parametros SET valor = %d where parametro = %s' % (int(set_max),"'LIMITE_MAX_TEMP'"))
+##		conn2.commit()
+##		conn2.close()
+##		return redirect('/lab_parametros')
 
 # somewhere to login
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username == 'user1' and password == 'user1_secret':
-            id = username.split('user')[1]
-            print id
-            user = User(id)
-            login_user(user)
-            return redirect(request.args.get("next") or url_for("index"))
-        else:
-            return abort(401)
-    else:
+	if request.method == 'POST':
+		#username = request.form['username']
+		#password = request.form['password']
+		user = User.get(request.form['username'])
+		#print user.username
+		if user and request.form['password'] == user.password:
+			login_user(user)
+			return redirect(request.args.get("next") or url_for("index"))
+		else:
+			return abort(401)
+	else:
 		return render_template('login.html')
 
 # somewhere to logout
 @app.route("/logout")
 @login_required
 def logout():
-    logout_user()
-    return Response('<p>Logged out</p>')
+	logout_user()
+	return Response('<p>Logged out</p>')
 
 
 # handle login failed
 @app.errorhandler(401)
 def page_not_found(e):
-    return Response('<p>Login failed</p>')
+	return Response('<p>Login failed</p>')
 
 
 # callback to reload the user object
 @login_manager.user_loader
 def load_user(userid):
-    return User(userid)
+	"""
+	Flask-Login user_loader callback.
+	The user_loader function asks this function to get a User Object or return
+	None based on the userid.
+	The userid was stored in the session environment by Flask-Login.
+	user_loader stores the returned User object in current_user during every
+	flask request.
+	"""
+	return User.get(userid)
 
 if __name__ == "__main__":
+	USERS = (("user1", "pass1"),
+		  ("cleiton", "teste")
+		  )
+
 	app.run(host='0.0.0.0')
